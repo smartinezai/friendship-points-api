970f8aea2640da44c71afc68396aefa0d35c1ebc
smartinezai <samescobar.ai@gmail.com>
2026-05-16 18:00:09 +0200

    add basic server for listening to requests

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
-import Fastify from "fastify";
+import app from "./app";
 
-const app = Fastify();
+const port = 3000;
 
-app.get("/health", async () => {
-    return {status:"ok"};
-});
+async function start() {
+    try{
+        await app.listen({
+            port,
+            host: "0.0.0.0",
+        });
+        console.log(`Server is running on port ${port}`);        
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
 
-
-export default app;
+start();
